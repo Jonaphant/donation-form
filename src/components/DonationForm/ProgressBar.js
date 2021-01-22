16a70e8eb9a9cc12068350361ac5497a07cea360
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 21:21:01 -0800

    Dynamically size progress bar based on donations

--- src/components/DonationForm/ProgressBar.js
+++ src/components/DonationForm/ProgressBar.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const ProgressBar = () => {
+const ProgressBar = ({ progressValue }) => {
+  const goalReached = progressValue >= 100;
+
   return (
     <div className="progress-container">
-      <div className="progress-bar"></div>
+      <div
+        className={`progress-bar ${goalReached && 'full-progress'}`}
+        style={{ width: `${progressValue}%` }}></div>
     </div>
   )
 }
