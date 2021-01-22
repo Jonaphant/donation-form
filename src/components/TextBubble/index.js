f0d98c53981b851f49014e0896b5928c075717d1
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 22:14:26 -0800

    Change text when donation goal reached. Add comma between large numbers

--- src/components/TextBubble/index.js
+++ src/components/TextBubble/index.js
@@ -2,16 +2,22 @@ import React, { useContext } from 'react';
 import './index.css';
 
 import { DONATION_GOAL } from '../../utils/constants';
+import { numberWithCommas } from '../../utils/index';
 import { DonationContext } from '../../provider/DonationContext';
 
 const TextBubble = () => {
   const { donated } = useContext(DonationContext);
-  const donationsNeeded = donated >= DONATION_GOAL ? 0 : DONATION_GOAL - donated;
+  const isDonationGoalReached = donated >= DONATION_GOAL;
+  const donationsNeeded = isDonationGoalReached ? 0 : DONATION_GOAL - donated;
 
   return (
     <div className="text-bubble-container">
       <div className="text-bubble">
-        <p className="message font-ibm-plex"><span className="font-bold">${donationsNeeded}</span> still needed to fund this project</p>
+        {isDonationGoalReached ? (
+          <p className="message font-ibm-plex font-bold">Our donation goal of ${numberWithCommas(DONATION_GOAL)} has been reached!</p>
+        ) : (
+          <p className="message font-ibm-plex"><span className="font-bold">${numberWithCommas(donationsNeeded)}</span> still needed to fund this project</p>
+        )}
       </div>
       <div className="point"></div>
     </div>
