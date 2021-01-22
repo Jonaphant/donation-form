5308e0e6552d5963e48d4134688c181b798c536b
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 17:47:19 -0800

    Create initial ProgressBar UI and some style tweaks to meet spec

--- src/components/DonationForm/index.js
+++ src/components/DonationForm/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './index.css';
 
 import TextBubble from '../TextBubble';
+import ProgressBar from './ProgressBar';
 
 const DonationForm = () => {
   return (
     <div>
       <TextBubble />
-      Donation Form Card
+      <ProgressBar />
+      <div className="donation-card">
+        <p className="font-bold font-poppins donation-header">Only four days left to fund this project</p>
+        <p className="font-ibm-plex">Join the <span className="font-bold">11</span> other donors who have already supported this project.</p>
+        {/* input form */}
+      </div>
     </div>
   )
 }
