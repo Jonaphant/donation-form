06c03832eaeea5b05fe2d44b388749e355a8ec90
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 21:00:46 -0800

    Core functionality to donate using input

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
 import './index.css';
 
-const Input = () => {
+const Input = ({onClick}) => {
+  const handleSubmit = (e) => {
+    // Validation
+
+    // Update donationAmount state
+    const donationAmount = parseInt(e.target.donated.value);
+    onClick(donationAmount)
+
+    // Reset form
+    e.target.reset()
+    e.preventDefault();
+  }
+
   return (
-    <div className="input-container">
+    <form className="input-container" onSubmit={handleSubmit}>
       <div className="dollar-icon-container">
         <span className="dollar-icon font-ibm-plex">$</span>
       </div>
-      <input type="number" className="input-field font-ibm-plex font-bold" placeholder="50"/>
-      <button type="submit" className="submit font-ibm-plex font-bold">Give Now</button>
-    </div>
+      <input
+        type="number"
+        name="donated"
+        className="input-field font-ibm-plex font-bold"
+        placeholder="50" 
+      />
+      <button
+        type="submit"
+        className="submit font-ibm-plex font-bold"
+      >
+        Give Now
+      </button>
+    </form>
   )
 }
 
