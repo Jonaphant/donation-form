4ac483a899145ab356d859a02eadd3a9c69a502c
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 16:17:04 -0800

    Change build directory back to public

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: "development",
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, ""),
+    path: path.resolve(__dirname, "public"),
     filename: "main.js"
   },
   target: "node",
