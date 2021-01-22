ddbd06e9963bff3cf89258b42702a82f4d9dcb22
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 19:55:19 -0800

    Finalized UI for TextBubble component

--- src/components/TextBubble/index.js
+++ src/components/TextBubble/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './index.css';
 
 const TextBubble = () => {
   return (
-    <div>
-      TextBubble
+    <div className="text-bubble-container">
+      <div className="text-bubble">
+        <p className="message font-ibm-plex"><span className="font-bold">$1250</span> still needed to fund this project</p>
+      </div>
+      <div className="point"></div>
     </div>
   )
 }
