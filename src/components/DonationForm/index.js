df483e5c9fd5f0da11f607b9f8b0df148b0e028f
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 20:59:56 -0800

    Add dynamic state value and pass callback to input component

--- src/components/DonationForm/index.js
+++ src/components/DonationForm/index.js
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './index.css';
+
+import { DonationContext } from '../../provider/DonationContext';
 
 import ProgressBar from './ProgressBar';
 import Input from '../Input';
 
 const DonationForm = () => {
+  const { setDonated, donors, setDonors } = useContext(DonationContext);
+
+  const handleDonationSubmit = (donationAmount) => {
+    const parsedAmount = parseInt(donationAmount);
+
+    if (!isNaN(parsedAmount)) {
+      // Update donated state
+      setDonated(prev => prev + parsedAmount);
+
+      // Increment donor state
+      setDonors(prev => prev + 1);
+    }
+  }
+
   return (
     <div>
       <ProgressBar />
       <div className="donation-card">
         <div>
           <p className="font-bold font-poppins donation-header">Only four days left to fund this project</p>
-          <p className="font-ibm-plex sub-header">Join the <span className="font-bold">11</span> other donors who have already supported this project.</p>
+          <p className="font-ibm-plex sub-header">Join the <span className="font-bold">{donors}</span> other donors who have already supported this project.</p>
         </div>
-        <Input />
+        <Input onClick={handleDonationSubmit} />
       </div>
     </div>
   )
