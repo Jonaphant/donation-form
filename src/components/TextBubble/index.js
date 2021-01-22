fc2ea22e813ce0786a254da2648b18d07f39b41a
Jonathan Sou <jonathansou7@gmail.com>
2021-01-21 20:21:25 -0800

    Calculate and display difference of donated amount and donation goal

--- src/components/TextBubble/index.js
+++ src/components/TextBubble/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './index.css';
 
+import { DONATION_GOAL } from '../../utils/constants';
+import { DonationContext } from '../../provider/DonationContext';
+
 const TextBubble = () => {
+  const { donated } = useContext(DonationContext);
+  const donationsNeeded = DONATION_GOAL - donated;
+
   return (
     <div className="text-bubble-container">
       <div className="text-bubble">
-        <p className="message font-ibm-plex"><span className="font-bold">$1250</span> still needed to fund this project</p>
+        <p className="message font-ibm-plex"><span className="font-bold">${donationsNeeded}</span> still needed to fund this project</p>
       </div>
       <div className="point"></div>
     </div>
